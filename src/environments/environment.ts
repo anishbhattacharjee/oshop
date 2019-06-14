ab3c8f6936ea7d6d4374eb8846458f30ff45ad62
anishbhattacharjee <33443758+anishbhattacharjee@users.noreply.github.com>
2019-06-14 14:11:27 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAovCt2wXIuKnuiDxY6DHtGLbthnS17XeQ",
-    authDomain: "oshop-95b51.firebaseapp.com",
-    databaseURL: "https://oshop-95b51.firebaseio.com",
-    projectId: "oshop-95b51",
-    storageBucket: "oshop-95b51.appspot.com",
-    messagingSenderId: "914913911318",
-    appId: "1:914913911318:web:560e4c24cd05cce7"
+    
   }
 };
 
