1117547749fa193d5ec8f6a67f22441245947188
anishbhattacharjee <anishbhattacharjee@gmail.com>
2019-06-14 14:09:26 +0530

    aa

---
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth' ;
+
+import * as firebase from 'firebase';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'bs-navbar',
+  templateUrl: './bs-navbar.component.html',
+  styleUrls: ['./bs-navbar.component.css']
+})
+export class BsNavbarComponent  {
+  user$: Observable<firebase.User>;
+
+  constructor( private afAuth: AngularFireAuth ) { 
+    this.user$ = afAuth.authState;
+
+  }
+
+
+
+  logout() {
+    this.afAuth.auth.signOut();
+
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAovCt2wXIuKnuiDxY6DHtGLbthnS17XeQ",
+    authDomain: "oshop-95b51.firebaseapp.com",
+    databaseURL: "https://oshop-95b51.firebaseio.com",
+    projectId: "oshop-95b51",
+    storageBucket: "oshop-95b51.appspot.com",
+    messagingSenderId: "914913911318",
+    appId: "1:914913911318:web:560e4c24cd05cce7"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
