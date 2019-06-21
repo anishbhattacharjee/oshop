5c76720ca8ea40d37a1455a207a8606615c578c5
anishbhattacharjee <anishbhattacharjee@gmail.com>
2019-06-21 12:42:39 +0530

    aaa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    
+    apiKey: '',
+    authDomain: 'oshop-95b51.firebaseapp.com',
+    databaseURL: 'https://oshop-95b51.firebaseio.com',
+    projectId: 'ohop-951',
+    storageBucket: 'ohop-95b51.appspot.com',
+    messagingSenderId: '91911gs318',
+    appId: '1:91495452451318:web:560hfhd05cce7'
   }
 };
 
